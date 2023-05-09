98dc85b5ad267db9b531a5df7852931f264763e2
bgomez <bgomez@Softcorpapp.com>
2023-05-09 08:36:00 -0400

    Creacion de funcion suma

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        int resultado = sumaVariables(13,15,27);
+        System.out.println("El resultado es: "+resultado);
+    }
+    static int sumaVariables(int a, int b, int c){
+        return a + b + c;
     }
 }
